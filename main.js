16670d3d6af02700edaa899cda21b6abfad93aed
Jatoran <adamtbandel@gmail.com>
2021-09-19 13:22:38 -0500

    first launch update

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
+
+
 $(function ()
 {
+
+    //Collapsible Menu
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
     $('img').on('click', function ()
     {
         var URL=this.getAttribute('src');
@@ -8,3 +27,13 @@ $(function ()
         // $(this).css('height',"120%");
     });
 });
+
+
+    $('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
+    
+    $("#menu-icon").on("click", function(){
+    $("nav").slideToggle();
+    $(this).toggleClass("active");
+});
+
+
